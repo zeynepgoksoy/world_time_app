2679f9bdce72a2609e435f931472ce022b989524
zeynepgoksoy <zeynepgoksoy98@gmail>
2024-03-27 15:55:03 +0300

    world time app first version-initState func added

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  const ChooseLocation({Key? key}) : super(key: key);
+
+  @override
+  State<ChooseLocation> createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+
+  int counter=0;
+  @override
+  void initState() {
+    super.initState();
+    print('initState function ran');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('build function rans');
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: ElevatedButton(
+        onPressed: (){
+          setState(() {
+            counter +=1;
+          });
+        },
+        child: Text('counter is $counter'),
+      ),
+    );
+  }
+}
